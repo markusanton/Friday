b210716d6abdc98427f584b9f1558b2d058c8e83
Opilane TTHK <opilane@tthk.ee>
2020-11-13 08:49:34 +0200

    first commit

---
+++ Friday/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string samplePath = @"C:\Users\opilane\samples";
+            string directoriesTxt = @"C:\Users\opilane\Friday\sampleDirectories.txt";
+            string filesTxt = @"C:\Users\opilane\Friday\sampleFiles.txt";
+
+            DirectoryWriter(directoriesTxt, samplePath);
+            FileWriter(filesTxt, samplePath);
+            Console.WriteLine("Written!!");
+
+        }
+        public static void DirectoryWriter(string fileName, string filePath)
+        {
+            string[] getNested = Directory.GetDirectories(filePath, "*", SearchOption.AllDirectories);
+            File.WriteAllLines(fileName, getNested);
+        }
+        public static void FileWriter(string fileName, string filePath)
+        {
+            string[] allFiles = Directory.GetFiles(filePath, "*", SearchOption.AllDirectories);
+            File.WriteAllLines(fileName, allFiles);
+        }
+    }
+}
